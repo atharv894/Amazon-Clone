194127e2ee1fdb15e6d1e33b34d520922c1d67ce
Atharv <atharv.sinh2006@gmail.com>
2026-02-18 17:54:26 +0530

    Use Modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+import { cart } from "../data/cart.js";
+//          OR
+// import { cart as MyCart } from "../data/cart.js";
+
 let productsHTML = "";
 products.forEach((product) => {
   const html = `
@@ -40,7 +44,7 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
               <img src="images/icons/checkmark.png" />
               Added
             </div>
@@ -51,6 +55,8 @@ products.forEach((product) => {
 });
 
 document.querySelector(".products-grid").innerHTML = productsHTML;
+
+let TimerId;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
@@ -70,8 +76,9 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     if (matchingItem === 0) {
       cart.push({
         productId: productId,
-        quantity: document.querySelector(`.js-quantity-selector-${productId}`)
-          .value,
+        quantity: Number(
+          document.querySelector(`.js-quantity-selector-${productId}`).value,
+        ),
       });
     }
 
@@ -83,5 +90,15 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     document.querySelector(".js-cart-quantity").innerHTML = totalQuantity;
     // console.log(totalQuantity);
     // console.log(cart);
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`,
+    );
+
+    addedMessage.classList.add("added-to-cart-visible");
+    clearTimeout(TimerId);
+    TimerId = setTimeout(() => {
+      addedMessage.classList.remove("added-to-cart-visible");
+    }, 2000);
   });
 });
